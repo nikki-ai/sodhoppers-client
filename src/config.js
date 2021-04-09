bf1ba4b030f3ccd8e98c416dbc538b1e6bcb4a78
nikki-ai <shony1198@gmail.com>
2021-04-09 17:02:42 -0500

    config changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
-  API_ENDPOINT: 'http://localhost:8000',
+  // API_ENDPOINT: 'http://localhost:8000',
+  API_ENDPOINT: 'https://calm-castle-34175.herokuapp.com',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
 
-module.exports = config;
+export default config;
