d53eb6f7a8686e3905b84c365f52800d763bc440
nikki-ai <shony1198@gmail.com>
2021-04-09 19:12:47 -0500

    put s back

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   // API_ENDPOINT: 'http://localhost:8000',
-  API_ENDPOINT: 'http://calm-castle-34175.herokuapp.com',
+  API_ENDPOINT: 'https://calm-castle-34175.herokuapp.com',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
 
